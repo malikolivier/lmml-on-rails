522ee5631537f5a9dae8fa6a0ea9d5705c95cda1
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-12-03 20:05:55 +0900

    Fix unit tests

--- test/controllers/autopsies_controller_test.rb
+++ test/controllers/autopsies_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AutopsiesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @autopsy = autopsies(:one)
+    @autopsy = autopsies(:completed_autopsy)
   end
 
   test "should get index" do
@@ -17,7 +17,7 @@ class AutopsiesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create autopsy" do
     assert_difference('Autopsy.count') do
-      post autopsies_url, params: { autopsy: { autopsy_date: @autopsy.autopsy_date, completed: @autopsy.completed, conclusion: @autopsy.conclusion, court_id: @autopsy.court_id, ending_time: @autopsy.ending_time, judge_id: @autopsy.judge_id, number: @autopsy.number, place_id: @autopsy.place_id, police_inspector_id: @autopsy.police_inspector_id, police_station_id: @autopsy.police_station_id, starting_time: @autopsy.starting_time, suspect_id: @autopsy.suspect_id, victim_id: @autopsy.victim_id } }
+      post autopsies_url, params: { autopsy: { autopsy_date: @autopsy.autopsy_date, completed: @autopsy.completed, court_id: @autopsy.court_id, ending_time: @autopsy.ending_time, judge_id: @autopsy.judge_id, number: @autopsy.number, place_id: @autopsy.place_id, police_inspector_id: @autopsy.police_inspector_id, police_station_id: @autopsy.police_station_id, starting_time: @autopsy.starting_time, suspect_id: @autopsy.suspect_id, victim_id: @autopsy.victim_id } }
     end
 
     assert_redirected_to autopsy_url(Autopsy.last)
@@ -34,7 +34,7 @@ class AutopsiesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update autopsy" do
-    patch autopsy_url(@autopsy), params: { autopsy: { autopsy_date: @autopsy.autopsy_date, completed: @autopsy.completed, conclusion: @autopsy.conclusion, court_id: @autopsy.court_id, ending_time: @autopsy.ending_time, judge_id: @autopsy.judge_id, number: @autopsy.number, place_id: @autopsy.place_id, police_inspector_id: @autopsy.police_inspector_id, police_station_id: @autopsy.police_station_id, starting_time: @autopsy.starting_time, suspect_id: @autopsy.suspect_id, victim_id: @autopsy.victim_id } }
+    patch autopsy_url(@autopsy), params: { autopsy: { autopsy_date: @autopsy.autopsy_date, completed: @autopsy.completed, court_id: @autopsy.court_id, ending_time: @autopsy.ending_time, judge_id: @autopsy.judge_id, number: @autopsy.number, place_id: @autopsy.place_id, police_inspector_id: @autopsy.police_inspector_id, police_station_id: @autopsy.police_station_id, starting_time: @autopsy.starting_time, suspect_id: @autopsy.suspect_id, victim_id: @autopsy.victim_id } }
     assert_redirected_to autopsy_url(@autopsy)
   end
 
