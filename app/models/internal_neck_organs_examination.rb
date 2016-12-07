17ea082715215e46c38523c2070783a4d856f338
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-12-08 01:28:48 +0900

    Fix rubocop

--- app/models/internal_neck_organs_examination.rb
+++ app/models/internal_neck_organs_examination.rb
@@ -23,14 +23,14 @@ class InternalNeckOrgansExamination < ApplicationRecord
   belongs_to :oesophagus_substance, class_name: Substance
   # Usually, subtances go from top to bottom, so if something is in the bronchi,
   # it is as well inside the trachea and the larynx.
-  for air_track_organ in Settings.air_track_organs
+  Settings.air_track_organs.each do |air_track_organ|
     belongs_to "#{air_track_organ}_substance".to_sym, class_name: Substance
   end
 
   def air_track_organs_description
     organ_names = []
     reached_air_track_organs.each do |organ|
-      organ_names.push(I18n.t "organs.#{organ}")
+      organ_names.push(I18n.t("organs.#{organ}"))
     end
     organ_names.join('・')
   end
@@ -39,11 +39,9 @@ class InternalNeckOrgansExamination < ApplicationRecord
 
   def reached_air_track_organs
     reached_organs = []
-    for air_track_organ in Settings.air_track_organs
+    Settings.air_track_organs.each do |air_track_organ|
       subtance = send("#{air_track_organ}_substance")
-      if subtance.nothing?
-        break
-      end
+      break if subtance.nothing?
       reached_organs.push(air_track_organ)
     end
     reached_organs
