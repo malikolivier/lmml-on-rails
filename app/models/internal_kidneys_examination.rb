f409b352280772731d0e0310d0a3075ca6d0703e
Malik Olivier Boussejra <malik@boussejra.com>
2016-12-22 13:38:28 +0900

    Completed kidneys display

--- app/models/internal_kidneys_examination.rb
+++ app/models/internal_kidneys_examination.rb
@@ -25,4 +25,34 @@ class InternalKidneysExamination < ApplicationRecord
   enum capsule_congestion: Settings.enums.three_scale_intensity, _prefix: true
   enum pelvis_congestion: Settings.enums.three_scale_intensity, _prefix: true
   enum pelvis_petechia: Settings.enums.five_scale_quantity, _prefix: true
+
+  def description
+    description = ''
+    description += "硬度は#{I18n.t "hardness.#{hardness}"}。" if hardness.present?
+    description += capsule_description
+    description += pelvis_description
+    description
+  end
+
+  def capsule_description
+    capsule_phrases = []
+    if capsule_removal.present?
+      capsule_phrases.push("被膜剥離#{I18n.t "difficulty.#{capsule_removal}"}")
+    end
+    if capsule_congestion.present?
+      capsule_phrases.push("鬱血#{I18n.t "intensity.#{capsule_congestion}"}")
+    end
+    capsule_phrases.to_sentence_with_comma_and_full_stop
+  end
+
+  def pelvis_description
+    pelvis_phrases = []
+    if pelvis_congestion.present?
+      pelvis_phrases.push("血管充盈#{I18n.t "intensity.#{pelvis_congestion}"}")
+    end
+    if pelvis_petechia.present?
+      pelvis_phrases.push("溢血点#{I18n.t "quantity.#{pelvis_congestion}"}")
+    end
+    pelvis_phrases.to_sentence_with_comma_and_full_stop
+  end
 end
