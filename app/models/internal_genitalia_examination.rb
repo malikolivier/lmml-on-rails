877a21c44e128148fbcb86282a7ffbb2701bfc0e
Malik Olivier Boussejra <malik@boussejra.com>
2016-12-22 22:33:40 +0900

    Write output for uterus

--- app/models/internal_genitalia_examination.rb
+++ app/models/internal_genitalia_examination.rb
@@ -18,10 +18,47 @@ class InternalGenitaliaExamination < ApplicationRecord
   belongs_to :examination, required: true
 
   enum endometrium_color: [:pale], _prefix: true
-  enum ovaries_presence: [:yes, :right_is_missing, :left_is_missing, :no],
+  enum ovaries_presence: [:yes, :left_is_missing, :right_is_missing, :no],
        _prefix: true
 
   def description
-    '子宮に位置や形の異常なし。子宮は長さ・ ㎝、幅底部で・ ㎝、頸部で・ ㎝。内膜は蒼白で異常なし。卵巣は左右共・　大で異常なし。'
+    description = ''
+    description += '子宮に位置や形の異常なし。' if examination.injuries.none?
+    description += uterus_dimension_description
+    description += "内膜は#{I18n.t("colors.#{endometrium_color}")}。" if endometrium_color.present?
+    description + ovaries_description
+  end
+
+  def uterus_dimension_description
+    phrases = []
+    phrases.push("長さ#{length}㎝") if length.present?
+    phrases.push("幅底部で#{fundal_width}㎝") if fundal_width.present?
+    phrases.push("頸部で#{cervical_width}㎝") if cervical_width.present?
+    if phrases.any?
+      "子宮は#{phrases.to_sentence_with_comma}。"
+    else
+      ''
+    end
+  end
+
+  def ovaries_description # rubocop:disable Metrics/CyclomaticComplexity
+    if ovaries_presence.present?
+      case ovaries_presence
+      when 'yes'
+        output = '卵巣は左右共'
+        output += "#{ovaries_distance}cm大" if ovaries_distance.present?
+        "#{output}。"
+      when 'left_is_missing'
+        '左卵巣は損失する。'
+      when 'right_is_missing'
+        '右卵巣は損失する。'
+      when 'no'
+        '卵巣はない。'
+      else
+        "Unhandled ovaries_presence: #{ovaries_presence}"
+      end
+    else
+      ''
+    end
   end
 end
