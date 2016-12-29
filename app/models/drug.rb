d6dc439ba8465372e7b1fe6cdb7ee8962dfc7d07
Malik Olivier Boussejra <malik@boussejra.com>
2016-12-29 22:17:21 +0900

    Use haml for triage test

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -16,18 +16,29 @@ class Drug < ApplicationRecord
     drug_translations.language(I18n.locale).take!
   end
 
-  # Define a method to get each of the translatable column
-  [:name, :long_name].each do |column|
-    define_method "translated_#{column}" do
-      begin
-        translation.send(column)
-      rescue ActiveRecord::RecordNotFound
-        "No translation found for drug ##{id} #{abbr}'s #{column} in #{I18n.locale}!"
-      end
-    end
+  # Define a method to get the name's translation
+  def translated_name
+    translation.name
+  rescue ActiveRecord::RecordNotFound
+    "No translation found for drug ##{id} #{abbr}'s name in #{I18n.locale}!"
+  end
+
+  # Fall back to name if long_name is not defined
+  def translated_long_name
+    translation.long_name.present? ? translation.long_name : translation.name
+  rescue ActiveRecord::RecordNotFound
+    "No translation found for drug ##{id} #{abbr}'s long_name in #{I18n.locale}!"
   end
 
   def triage_concentration_threshold
     triage_supported_drug.concentration_threshold
+  end
+
+  def description
+    if abbr.present?
+      "#{abbr}：#{translated_long_name}"
+    else
+      translated_long_name
+    end
   end
 end
