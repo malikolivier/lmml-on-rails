a626747c753dbf8c109e4a3278e1a34366e65cdf
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-02 19:17:03 +0900

    Fix rubocop

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get people_index_url
     assert_response :success
   end
-
 end
