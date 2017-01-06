ddb511a51aa2b0072469eeb82952bc53f93d137d
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-07 05:23:24 +0900

    Add support for rectal temperature

--- app/controllers/external_outline_examinations_controller.rb
+++ app/controllers/external_outline_examinations_controller.rb
@@ -36,6 +36,7 @@ class ExternalOutlineExaminationsController < ApplicationController
 
   def update_params
     params.require(:external_outline_examination)
-          .permit(:id, :sex, :partial_body, :height, :weight, :constitution)
+          .permit(:id, :sex, :partial_body, :height, :weight, :constitution,
+                  :rectal_temperature, :room_temperature)
   end
 end
