31bb0706dc89ed4dd8fa9a2563cd5ee7c9c89c7b
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-07 21:02:58 +0900

    Can edit array

--- app/assets/javascripts/external_outline_examinations.js
+++ app/assets/javascripts/external_outline_examinations.js
@@ -4,11 +4,25 @@
 $(function() {
   if (document.getElementById('external_outline_examination') === null) return;
 
-  var autopsyVm = LMML.loadVueModel('external_outline_examination', {
+  var vm = LMML.loadVueModel('external_outline_examination', {
     update_url: 'external_outline_examinations',
     data: {
     },
     methods: {
+      add_livor_mortis: function() {
+        this.$http.post('/livor_mortis', {
+          external_outline_examination_id: this.id
+        }).then(function(response) {
+          console.log(response);
+        }, function(response) {
+          console.error(response)
+          var errorElement = document.getElementById('external_outline_examination_errors');
+          if (response.body.errors)
+            errorElement.innerHTML = response.body.errors;
+          else
+            errorElement.innerHTML = response.body;
+        });
+      }
     }
   });
 
--- app/assets/javascripts/load-vue-models.js
+++ app/assets/javascripts/load-vue-models.js
@@ -26,57 +26,92 @@ LMML.loadVueModel = function(model, options = {}) {
         };
       } else {
         var joinedNames = names.join('.');
-        watch[joinedNames] = _.debounce(
-          function onIndividualChange(newValue, oldValue) {
-            console.log(`Updating ${model}'s ${joinedNames} to: ${newValue}`);
-            var params = {}
-            var scopedParams = params;
-            var scopedData = data;
-            for (var i = 0; i < names.length - 1; i++) {
-              scopedParams[names[i]] = {};
-              scopedParams = scopedParams[names[i]];
-              scopedData = scopedData[names[i]];
-            }
-            scopedParams[names[names.length - 1]] = newValue;
-            // Add ID so that rails update this record
-            if (scopedData.id) scopedParams.id = scopedData.id;
-            this.$http[options.http_verb](options.update_url, {[model]: params})
-            .then(function(response) {
-              document.getElementById(model + '_preview').innerHTML = response.body;
-              document.getElementById(model + '_errors').innerHTML = '';
-            }, function(response) {
-              console.error(response)
-              var errorElement = document.getElementById(model + '_errors');
-              if (response.body.errors)
-                errorElement.innerHTML = response.body.errors;
-              else
-                errorElement.innerHTML = response.body;
-            });
-          },
-          500
-        )
-      }
-      for (var i = 0; i < names.length; i++) {
-        var name = names[i];
-        // Check if name is an array
-        var array_match = name.match(/^(\w+)\[([0-9]+)\]$/);
-        var index = null;
-        if (array_match) {
-          name = array_match[1];
-          index = array_match[2];
-          scoped_data[name] =  scoped_data[name] || [];
-          scoped_data = scoped_data[name];
-          name = index;
-        }
-        if (i === names.length - 1) {
-          if (this.type == 'checkbox') {
-            scoped_data[name] = this.checked;
-          } else {
-            scoped_data[name] = this.value;
-          }
+        var array_index = names.findIndex(function(name) {
+          return name.match(/^(\w+)\[([0-9]+)\]$/);
+        });
+        if (array_index == -1) {
+          watch[joinedNames] = _.debounce(
+            function onIndividualChange(newValue, oldValue) {
+              console.log(`Updating ${model}'s ${joinedNames} to: ${newValue}`);
+              var params = {};
+              var scopedParams = params;
+              var scopedData = data;
+              for (var i = 0; i < names.length - 1; i++) {
+                scopedParams[names[i]] = {};
+                scopedParams = scopedParams[names[i]];
+                scopedData = scopedData[names[i]];
+              }
+              scopedParams[names[names.length - 1]] = newValue;
+              // Add ID so that rails update this record
+              if (scopedData.id) scopedParams.id = scopedData.id;
+              this.$http[options.http_verb](options.update_url, {[model]: params})
+              .then(function(response) {
+                document.getElementById(model + '_preview').innerHTML = response.body;
+                document.getElementById(model + '_errors').innerHTML = '';
+              }, function(response) {
+                console.error(response)
+                var errorElement = document.getElementById(model + '_errors');
+                if (response.body.errors)
+                  errorElement.innerHTML = response.body.errors;
+                else
+                  errorElement.innerHTML = response.body;
+              });
+            },
+            500
+          )
         } else {
-          scoped_data[name] = scoped_data[name] || {};
-          scoped_data = scoped_data[name];
+          var array_name = names[array_index].match(/^(\w+)\[[0-9]+\]$/)[1];
+          if (!watch[array_name]) {
+            watch[array_name] = {
+              handler: _.debounce(
+                function onArrayChange(newValue, oldValue) {
+                  console.log(`Updating array ${array_name}...`);
+                  console.log(newValue);
+                  console.log(oldValue);
+                  this.$http[options.http_verb](options.update_url, {
+                    [model]: {
+                      [array_name]: newValue
+                    }
+                  }).then(function(response) {
+                    document.getElementById(model + '_preview').innerHTML = response.body;
+                    document.getElementById(model + '_errors').innerHTML = '';
+                  }, function(response) {
+                    console.error(response)
+                    var errorElement = document.getElementById(model + '_errors');
+                    if (response.body.errors)
+                      errorElement.innerHTML = response.body.errors;
+                    else
+                      errorElement.innerHTML = response.body;
+                  });
+                },
+                500
+              ),
+              deep: true
+            };
+          }
+        }
+        for (var i = 0; i < names.length; i++) {
+          var name = names[i];
+          // Check if name is an array
+          var array_match = name.match(/^(\w+)\[([0-9]+)\]$/);
+          var index = null;
+          if (array_match) {
+            name = array_match[1];
+            index = array_match[2];
+            scoped_data[name] =  scoped_data[name] || [];
+            scoped_data = scoped_data[name];
+            name = index;
+          }
+          if (i === names.length - 1) {
+            if (this.type == 'checkbox') {
+              scoped_data[name] = this.checked;
+            } else {
+              scoped_data[name] = this.value;
+            }
+          } else {
+            scoped_data[name] = scoped_data[name] || {};
+            scoped_data = scoped_data[name];
+          }
         }
       }
     }
@@ -110,6 +145,7 @@ LMML.loadVueModel = function(model, options = {}) {
       methods[key] = value;
     });
   }
+
 
   var newModelVm = new Vue({
     el: '#' + model,
