1db0afa9863b80a7f65b3c1c4740f527ae97f0f4
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2017-01-09 02:32:51 +0900

    Reset database for tests

--- spec/magic_lamp.rb
+++ spec/magic_lamp.rb
@@ -3,4 +3,14 @@ MagicLamp.define(controller: AutopsiesController) do
     @autopsy = Autopsy.new
     render 'new'
   end
+
+  fixture(name: 'edit_empty') do
+    @autopsy = Autopsy.new
+    render 'edit'
+  end
+
+  fixture(name: 'edit_completed') do
+    @autopsy = Autopsy.find_by(number: '000001')
+    render 'edit'
+  end
 end
