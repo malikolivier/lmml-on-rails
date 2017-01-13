039432f8b24e38d9b57b1a599fde8dab0c8cb7d0
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-13 22:50:08 +0900

    VueFormBuilder: support v-model even for nested fields

--- app/helpers/vue_form_builder.rb
+++ app/helpers/vue_form_builder.rb
@@ -1,13 +1,13 @@
 # Extend FormBuilder to build Vue-friendly forms
 class VueFormBuilder < ActionView::Helpers::FormBuilder
   def text_field(object_name, label_name = nil, options = {})
-    options[:'v-model'] ||= object_name
+    options[:'v-model'] ||= v_model_value(object_name)
     add_form_control(options)
     wrap_with_label(object_name, super(object_name, options), label_name)
   end
 
   def select(method, choices = nil, options = {}, html_options = {}, &block)
-    html_options[:'v-model'] ||= method
+    html_options[:'v-model'] ||= v_model_value(method)
     add_form_control(html_options)
     input_html = super(method, choices, options, html_options, &block)
     wrap_with_label(method, input_html)
@@ -16,7 +16,7 @@ class VueFormBuilder < ActionView::Helpers::FormBuilder
   # rubocop:disable ParameterLists
   def collection_select(method, collection, value_method, text_method,
                         options = {}, html_options = {})
-    html_options[:'v-model'] ||= method
+    html_options[:'v-model'] ||= v_model_value(method)
     add_form_control(html_options)
     input_html = super(method, collection, value_method, text_method,
                        options, html_options)
@@ -24,12 +24,12 @@ class VueFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def number_field(object_name, options = {})
-    options[:'v-model.number'] ||= object_name
+    options[:'v-model.number'] ||= v_model_value(object_name)
     super(object_name, options)
   end
 
   def time_field_v(object_name, options = {})
-    options[:'v-model'] ||= object_name
+    options[:'v-model'] ||= v_model_value(object_name)
     if object.send(object_name).present?
       options[:value] ||= object.send(object_name).strftime('%H:%M')
     end
@@ -37,7 +37,7 @@ class VueFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def date_field_v(object_name, options = {})
-    options[:'v-model'] ||= object_name
+    options[:'v-model'] ||= v_model_value(object_name)
     date_field(object_name, options)
   end
 
@@ -60,5 +60,16 @@ class VueFormBuilder < ActionView::Helpers::FormBuilder
     class_def << " #{html_options[:class]}" if html_options[:class].present?
     html_options[:class] = class_def
     html_options
+  end
+
+  # Return v-model attributes for current object. Work even for nested objects.
+  def v_model_value(object_name)
+    matched_attributes = @object_name.match(/^\w+\[(.*)\]$/) { |m| m[1] }
+    if matched_attributes.nil?
+      object_name
+    else
+      methods = matched_attributes.split('][')
+      "#{methods.join('.')}.#{object_name}"
+    end
   end
 end
