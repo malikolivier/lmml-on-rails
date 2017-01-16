1e55fed6856f504ff46abd585886ef0acbaa0b63
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-17 01:31:12 +0900

    Heavily simplify JS for handling nested models

--- app/assets/javascripts/01-inflection.js
+++ app/assets/javascripts/01-inflection.js
@@ -1,7 +1,11 @@
 /* eslint no-extend-native: 0 */
-String.prototype.pluralize = function pluralize () {
+String.prototype.pluralize = function pluralize() {
   if (this.endsWith('y')) {
     return this.replace(/^(.*)y$/, '$1ies')
+  } else if (this.toString() === 'livor_mortis') {
+    return 'livores_mortis'
+  } else if (this.toString() === 'rigor_mortis') {
+    return 'rigores_mortis'
   } else {
     return `${this}s`
   }
--- app/assets/javascripts/external_outline_examinations.js
+++ app/assets/javascripts/external_outline_examinations.js
@@ -13,36 +13,8 @@ LMML.loaders.external_outline_examination = function () {
   var model = 'external_outline_examination'
   LMML.loadVueModel(model, {
     methods: {
-      add_livor_mortis: function () {
-        new Promise((resolve, reject) => {
-          if (this.id !== '') {
-            resolve()
-          } else {
-            this.$http.post(LMML.models_url(model))
-            .then(function (response) {
-              this.id = response.body.model.id
-              resolve()
-            }, reject)
-          }
-        })
-        .then(() => {
-          return this.$http.post('/livores_mortis', {
-            external_outline_examination_id: this.id
-          })
-        })
-        .then((response) => {
-          this.livores_mortis_attributes.push(response.body)
-        }, LMML.httpErrorHandler(model))
-      },
-      delete_livor_mortis: function (livorMortis) {
-        this.$http.delete(`/livores_mortis/${livorMortis.id}`)
-        .then(function (response) {
-          var i = this.livores_mortis_attributes.findIndex(function (livor) {
-            return livor.id === livorMortis.id
-          })
-          this.livores_mortis_attributes.splice(i, 1)
-        }, LMML.httpErrorHandler(model))
-      },
+      add_livor_mortis: LMML.add_('livor_mortis', model),
+      delete_livor_mortis:  LMML.delete_('livor_mortis', model),
       add_rigor_mortis: function () {
         throw new Error('Not implemented')
       },
