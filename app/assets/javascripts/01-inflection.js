ace4cc49d73f75a904dcec178fe019c76a912185
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-17 12:38:02 +0900

    Add unit tests for JS inflections

--- app/assets/javascripts/01-inflection.js
+++ app/assets/javascripts/01-inflection.js
@@ -3,9 +3,9 @@
 LMML.pluralize = function pluralize (word) {
   if (word.endsWith('y')) {
     return word.replace(/^(.*)y$/, '$1ies')
-  } else if (word.toString() === 'livor_mortis') {
+  } else if (word === 'livor_mortis') {
     return 'livores_mortis'
-  } else if (word.toString() === 'rigor_mortis') {
+  } else if (word === 'rigor_mortis') {
     return 'rigores_mortis'
   } else {
     return `${word}s`
---
+++ spec/javascripts/pluralize_spec.js
@@ -0,0 +1,14 @@
+/* eslint-env jasmine */
+/* global LMML */
+
+describe('Pluralize', function () {
+  it('inflects latin plural', function () {
+    expect(LMML.pluralize('livor_mortis')).toBe('livores_mortis')
+  })
+  it('inflects autopsy', function () {
+    expect(LMML.pluralize('autopsy')).toBe('autopsies')
+  })
+  it('inflects external_examination', function () {
+    expect(LMML.pluralize('external_examination')).toBe('external_examinations')
+  })
+})
