706f63f3cf1c611a74a64c74a0615573fc026ea9
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-18 13:14:55 +0900

    Add hair info

--- app/controllers/external_head_examinations_controller.rb
+++ app/controllers/external_head_examinations_controller.rb
@@ -3,7 +3,8 @@ class ExternalHeadExaminationsController < ExaminationsController
 
   def update_params
     params.require(:external_head_examination)
-          .permit(:id,
+          .permit(:id, :hair_natural_color, :hair_dyed_color,
+                  :top_hair_length, :side_hair_length,
                   examination_attributes: [:note])
   end
 end
