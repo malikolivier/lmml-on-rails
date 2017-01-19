00b76745e719cbaab970db85612542c903cc7f25
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2017-01-19 10:49:17 +0900

    Fix rubocop

--- app/controllers/external_face_examinations_controller.rb
+++ app/controllers/external_face_examinations_controller.rb
@@ -8,19 +8,14 @@ class ExternalFaceExaminationsController < ExaminationsController
   private
 
   def update_params
+    eye_attributes = [:id, :closed, :aperture,
+                      :eyelid_congestion, :eyelid_petechia, :eyelid_note,
+                      :conjunctiva_congestion, :conjunctiva_petechia,
+                      :conjunctiva_note,
+                      :cornea, :pupil, :diameter]
     params.require(:external_face_examination)
           .permit(:id, :congestion, :swelling, :nasal_cavity_content,
-                  external_eye_examinations_attributes: [:id, :closed,
-                                                         :aperture,
-                                                         :eyelid_congestion,
-                                                         :eyelid_petechia,
-                                                         :eyelid_note,
-                                                         :conjunctiva_congestion,
-                                                         :conjunctiva_petechia,
-                                                         :conjunctiva_note,
-                                                         :cornea,
-                                                         :pupil,
-                                                         :diameter],
+                  external_eye_examinations_attributes: eye_attributes,
                   external_mouth_examination_attributes: [:id, :closed,
                                                           :petechia,
                                                           :tongue_tip,
