ff99354c6cce6c1e480c5befcf05561484eae73a
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2017-01-19 19:49:57 +0900

    Update face controller: allow to update mouth foreign fluids

--- app/controllers/external_face_examinations_controller.rb
+++ app/controllers/external_face_examinations_controller.rb
@@ -13,13 +13,13 @@ class ExternalFaceExaminationsController < ExaminationsController
                       :conjunctiva_congestion, :conjunctiva_petechia,
                       :conjunctiva_note,
                       :cornea, :pupil, :diameter]
+    mouth_attributes = [:id, :closed, :petechia, :tongue_tip, :note,
+                        foreign_fluids_attributes: [:id, :name, :color,
+                                                    :description]]
     params.require(:external_face_examination)
           .permit(:id, :congestion, :swelling, :nasal_cavity_content,
                   external_eye_examinations_attributes: eye_attributes,
-                  external_mouth_examination_attributes: [:id, :closed,
-                                                          :petechia,
-                                                          :tongue_tip,
-                                                          :note],
+                  external_mouth_examination_attributes: mouth_attributes,
                   examination_attributes: [:note])
   end
 end
