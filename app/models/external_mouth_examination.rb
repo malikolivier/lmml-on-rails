13213d58b0ff4e7a41588e96ee552ca70ff1ec86
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2017-01-22 18:49:50 +0900

    Order teeth

--- app/models/external_mouth_examination.rb
+++ app/models/external_mouth_examination.rb
@@ -21,7 +21,7 @@ class ExternalMouthExamination < ApplicationRecord
 
   has_many :in_mouth_foreign_fluids
   has_many :foreign_fluids, through: :in_mouth_foreign_fluids
-  has_many :tooth_examinations
+  has_many :tooth_examinations, -> { order(:position, :rank) }
   alias_method :teeth, :tooth_examinations
 
   has_many :mouth_photograph_takings
