658af520ce3070714d68fce71342a3a4beed6168
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2017-01-22 18:04:52 +0900

    Authorize tooth attributes

--- app/controllers/external_face_examinations_controller.rb
+++ app/controllers/external_face_examinations_controller.rb
@@ -15,7 +15,9 @@ class ExternalFaceExaminationsController < ExaminationsController
                       :cornea, :pupil, :diameter]
     mouth_attributes = [:id, :closed, :aperture, :petechia, :tongue_tip, :note,
                         foreign_fluids_attributes: [:id, :name, :color, :odor,
-                                                    :description]]
+                                                    :description],
+                        tooth_examinations_attributes: [:id, :position, :rank,
+                                                        :condition, :note]]
     params.require(:external_face_examination)
           .permit(:id, :congestion, :swelling, :nasal_cavity_content,
                   external_eye_examinations_attributes: eye_attributes,
