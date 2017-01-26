645b78d6758b0439b7fc52c2b074e6bd54b96f4a
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-26 16:30:04 +0900

    Simplify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,16 +10,19 @@ Rails.application.routes.draw do
     put :update
     get :edit_internal
     get :edit_analyses
-    [:external_outline, :external_head, :external_face, :external_neck,
-     :external_trunk, :external_back, :external_upper_limbs,
-     :external_lower_limbs, :external_genitalia, :external_anus,
-     :internal_cranium]
-    .each do |examination_type|
-      key_string_plural = "#{examination_type}_examination".pluralize
+    define_examination_routes = lambda { |category, part|
+      key_string_plural = "#{category}_#{part}_examination".pluralize
       resources key_string_plural, only: [:create, :new]
       put key_string_plural, to: "#{key_string_plural}#update"
       # This route is used for indenpendent debugging of examination form (like /new)
       get "#{key_string_plural}/edit", to: "#{key_string_plural}#edit"
+    }
+    [:outline, :head, :face, :neck, :trunk, :back, :upper_limbs, :lower_limbs,
+     :genitalia, :anus].each do |part|
+      define_examination_routes.call(:external, part)
+    end
+    [:cranium].each do |part|
+      define_examination_routes.call(:internal, part)
     end
   end
   post 'autopsies/preview', to: 'autopsies#preview'
