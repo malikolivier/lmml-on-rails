d6fa949186f5c7e57b02817e91cb63048c9363b8
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-27 10:53:28 +0900

    Finish brain input support

--- app/controllers/internal_brain_examinations_controller.rb
+++ app/controllers/internal_brain_examinations_controller.rb
@@ -1,2 +1,9 @@
 class InternalBrainExaminationsController < ExaminationsController
+  private
+
+  def update_params
+    params.require(:internal_brain_examination)
+          .permit(:weight, :pia_congestion, :blood_spots,
+                  examination_attributes: [:note])
+  end
 end
