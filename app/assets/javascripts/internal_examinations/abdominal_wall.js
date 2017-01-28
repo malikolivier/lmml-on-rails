ee00bd99f1b1f19408ecca5d0b4130669f5c41ce
Malik Olivier Boussejra <malik@boussejra.com>
2017-01-28 21:01:46 +0900

    Fixing nested array forms

--- app/assets/javascripts/internal_examinations/abdominal_wall.js
+++ app/assets/javascripts/internal_examinations/abdominal_wall.js
@@ -8,5 +8,12 @@ $(function () {
 
 LMML.loaders.internal_abdominal_wall_examination = function () {
   var model = 'internal_abdominal_wall_examination'
-  LMML.loadVueModel(model)
+  LMML.loadVueModel(model, {
+    methods: {
+      add_pleura_foreign_fluid: LMML.add_('pleura_foreign_fluid', model),
+      delete_pleura_foreign_fluid: LMML.delete_('pleura_foreign_fluid', model),
+      add_peritoneum_foreign_fluid: LMML.add_('peritoneum_foreign_fluid', model),
+      delete_peritoneum_foreign_fluid: LMML.delete_('peritoneum_foreign_fluid', model)
+    }
+  })
 }
