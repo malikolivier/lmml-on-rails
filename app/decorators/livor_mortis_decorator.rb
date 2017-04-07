1e82f739ecc4dd46322c06f02630d2cd879556c1
Malik Olivier Boussejra <malik@boussejra.com>
2017-04-08 00:49:04 +0900

    Completed livor mortis decorator

--- app/decorators/livor_mortis_decorator.rb
+++ app/decorators/livor_mortis_decorator.rb
@@ -5,5 +5,11 @@ class LivorMortisDecorator < ApplicationDecorator
       value = LivorMortis.translated_enum_value(:intensity, intensity)
       I18n.t('livores_mortis.intensity_description', intensity: value)
     end
+
+    def color_description(color)
+      return '' if color.blank?
+      value = LivorMortis.translated_enum_value(:color, color)
+      I18n.t('livores_mortis.color_description', color: value)
+    end
   end
 end
--- app/decorators/livores_mortis_decorator.rb
+++ app/decorators/livores_mortis_decorator.rb
@@ -7,22 +7,7 @@ class LivoresMortisDecorator < ApplicationCollectionDecorator
         pb << t('.body_parts_absent_description', body_parts: parts)
         next
       end
-      int_desc = LivorMortisDecorator.intensity_description(intensity)
-      livores.group_by(&:color).each do |color, livores_|
-        t_color = LivorMortis.translated_enum_value(:color, color) || ''
-        color_desc = t('.color_description', color: t_color)
-        livores_.group_by(&:on_finger_pressure).each do |finger, livores__|
-          parts = body_parts(livores__)
-          parts_desc = t('.body_parts_description', body_parts: parts)
-          first_part = t('.1st_part_description',
-                         body_parts: parts_desc, color: color_desc,
-                         intensity: int_desc)
-          finger_desc = LivorMortis.translated_enum_value(:on_finger_pressure,
-                                                          finger) || ''
-          pb << t('.single_description', '1st_part': first_part,
-                                         finger: finger_desc)
-        end
-      end
+      build_single_description(pb, intensity, livores)
     end
     t('.complete_description', livores_mortis: pb.to_sentence_no_dot)
   end
@@ -33,5 +18,28 @@ class LivoresMortisDecorator < ApplicationCollectionDecorator
     h.join_words(livores_mortis.map do |livor|
                    livor.translated_enum_value(:position)
                  end)
+  end
+
+  def build_single_description(pb, intensity, livores_mortis)
+    intensity_desc = LivorMortisDecorator.intensity_description(intensity)
+    livores_mortis.group_by(&:color).each do |color, livores|
+      color_desc = LivorMortisDecorator.color_description(color)
+      livores.group_by(&:on_finger_pressure).each do |finger, livores_|
+        finger_desc = LivorMortis.translated_enum_value(:on_finger_pressure,
+                                                        finger)
+        pb << single_description_helper(livores_, color_desc, intensity_desc,
+                                        finger_desc)
+      end
+    end
+  end
+
+  def single_description_helper(livores_mortis, color_desc, intensity_desc,
+                                finger_desc)
+    parts = body_parts(livores_mortis)
+    parts_desc = t('.body_parts_description', body_parts: parts)
+    first_part = t('.1st_part_description',
+                   body_parts: parts_desc, color: color_desc,
+                   intensity: intensity_desc)
+    t('.single_description', '1st_part': first_part, finger: finger_desc)
   end
 end
