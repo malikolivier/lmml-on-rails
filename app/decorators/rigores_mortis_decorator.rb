4cd7415cb2e5a1aee5c8acb0c6ec398684e65d43
Malik Olivier Boussejra <malik@boussejra.com>
2017-04-08 02:02:53 +0900

    RigorMortisDecorator: Added category support

--- app/decorators/rigores_mortis_decorator.rb
+++ app/decorators/rigores_mortis_decorator.rb
@@ -1,22 +1,34 @@
 class RigoresMortisDecorator < ApplicationCollectionDecorator
   def description
-    pb = PhraseBuilder.new(only_comma: true)
-    RigorMortis.intensities.each do |intensity, _|
-      joints = h.join_words(joints_by_intensity(intensity))
-      next if joints.empty?
-      int_desc = RigorMortisDecorator.intensity_description(intensity)
-      joints_desc = t('.joints_description', joints: joints)
-      pb << t('.single_description', intensity: int_desc, joints: joints_desc)
+    object.group_by(&:category).map do |category, rigores|
+      description_by_category(category, rigores)
     end
-    return '' if pb.empty?
-    t('.complete_description', single_description: pb.to_sentence_no_dot)
   end
 
   private
 
-  def joints_by_intensity(intensity)
-    object.send(intensity).map do |rigor_mortis|
-      rigor_mortis.translated_enum_value(:joint)
+  def joints_description(rigores = self)
+    joints = h.join_words(rigores.map do |rigor_mortis|
+                            rigor_mortis.translated_enum_value(:joint)
+                          end)
+    if joints.empty?
+      ''
+    else
+      t('.joints_description', joints: joints)
     end
+  end
+
+  def description_by_category(category, rigores_mortis = self)
+    pb = PhraseBuilder.new(only_comma: true)
+    category = RigorMortis.translated_enum_value(:category, category)
+    rigores_mortis.group_by(&:intensity).each do |intensity, rigores|
+      joints_desc = joints_description(rigores)
+      next if joints_desc.empty?
+      int_desc = RigorMortisDecorator.intensity_description(intensity)
+      pb << t('.single_description', intensity: int_desc, joints: joints_desc)
+    end
+    return '' if pb.empty?
+    t('.complete_description', single_description: pb.to_sentence_no_dot,
+                               category: category)
   end
 end
