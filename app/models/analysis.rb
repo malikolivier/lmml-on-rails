7afd8aced41bfdc68ac1d653605c2ff951fc2a6e
Malik Olivier Boussejra <malik@boussejra.com>
2017-04-09 12:14:56 +0900

    Try things...
    Does not work
    http://stackoverflow.com/questions/30789334/express-sql-union-query-rails-way

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -15,6 +15,26 @@ class Analysis < ApplicationRecord
   belongs_to :autopsy, required: true
   belongs_to :analysis_type, required: true
 
+  default_scope do
+    analyses = arel_table
+    select('`analyses`.*, `analysis_types`.`placement`, `analysis_others`.`placement`').from(
+      analyses.create_table_alias(
+        built_in_analyses.union(other_analyses),
+        table_name
+      )
+    ).order('`analysis_types`.`placement`, `analysis_others`.`placement`')
+  end
+  scope :built_in_analyses, -> {
+    select("`analyses`.*, `analysis_types`.`placement`, 0 AS '`analysis_others`.`placement`'")
+      .joins(:analysis_type).where.not(analysis_types: { name: 'other'})
+  }
+  scope :other_analyses, -> {
+    select('`analyses`.*, `analysis_types`.`placement`, `analysis_others`.`placement`')
+      .joins(:analysis_type)
+      .joins(:analysis_other)
+      .where(analysis_types: { name: 'other'})
+  }
+
   def get
     analysis_type.this_analysis_model.find_by!(analysis: self)
   end
