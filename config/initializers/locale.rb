71352902070ddfa51aeb48b0287dd29dddde273d
Malik Olivier Boussejra <malik@boussejra.com>
2017-04-10 00:13:40 +0900

    Set up default_url_options

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,3 +1,17 @@
 I18n.enforce_available_locales = false
 I18n.config.default_locale = :ja
-Rails.application.routes.default_url_options = { locale: I18n.locale }
+
+# Set default_url_options
+# Ensure that the locale choice is persistent across requests
+# What was written in the docs does not work for rails 5
+# https://apidock.com/rails/ActionController/Base/default_url_options
+# Created an issue: https://github.com/rails/rails/issues/28714
+module ActionDispatch
+  module Routing
+    module UrlFor
+      def default_url_options
+        { locale: I18n.locale }
+      end
+    end
+  end
+end
