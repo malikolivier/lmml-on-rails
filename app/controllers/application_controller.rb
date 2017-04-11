8e2aa4df9578020ebe4b0b2094de8fb2a17f2eaa
Malik Olivier Boussejra <malik@boussejra.com>
2017-04-12 08:34:30 +0900

    ApplicationController: Reliably set locale for each request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,19 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    # if params[:locale] is nil then I18n.default_locale will be used
-    I18n.locale = params[:locale]
+    # if params[:locale] is nil or is unavailable then
+    # I18n.default_locale will be used
+    if params[:locale].blank?
+      I18n.locale = I18n.default_locale
+    elsif locale_available?
+      I18n.locale = params[:locale]
+    else
+      Rails.logger.debug("Unsupported locale: #{params[:locale]}")
+      I18n.locale = I18n.default_locale
+    end
+  end
+
+  def locale_available?
+    I18n.available_locales.include?(params[:locale].to_sym)
   end
 end
