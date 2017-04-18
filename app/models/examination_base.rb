acfb57c855e2bde05bf827143d44196e72d7d719
Malik Olivier Boussejra <malik@boussejra.com>
2017-04-18 19:07:28 +0900

    Define convenient method on DualExaminationBase class
    This method allows to get the parent examination of a collection
    of examinations without having to grope for the object type.
    Hurray duck typing!

--- app/models/examination_base.rb
+++ app/models/examination_base.rb
@@ -6,4 +6,14 @@ class ExaminationBase < ApplicationRecord
   accepts_nested_attributes_for :examination, update_only: true
 
   includes_in_json :examination
+
+  class << self
+    # Convenient function to get the examination out of ActiveRecord::Relation
+    # on an instance of a class deriving from ExaminationBase.
+    # When the items in the relation all holds the same examination, returns
+    # the examination. Returns nil if the relation is empty.
+    def examination
+      take.examination if count > 0
+    end
+  end
 end
