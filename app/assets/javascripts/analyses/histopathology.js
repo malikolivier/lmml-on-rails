8ba2fbc26024d298e17688fcdf9c31e244ec6e3d
Malik Olivier Boussejra <malik@boussejra.com>
2017-06-06 11:44:42 +0900

    histopathology.js: Implement Add/Delete functions

--- app/assets/javascripts/analyses/histopathology.js
+++ app/assets/javascripts/analyses/histopathology.js
@@ -10,6 +10,10 @@ LMML.loaders.analysis_histopathology = function () {
   var model = 'analysis_histopathology'
   LMML.loadVueModel(model, {
     methods: {
+      add_analysis_histopathology_on_organ:
+        LMML.add_('analysis_histopathology_on_organ', model),
+      delete_analysis_histopathology_on_organ:
+        LMML.delete_('analysis_histopathology_on_organ', model)
     }
   })
 }
