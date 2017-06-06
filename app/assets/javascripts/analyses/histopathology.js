2c9eab7732b2edffd50c17cfa5ac0b46838a054d
Malik Olivier Boussejra <malik@boussejra.com>
2017-06-06 12:59:48 +0900

    histopathology.js: Can only edit one organ at a time
    If other_organ is set, organ_id is unset and vice-versa.

--- app/assets/javascripts/analyses/histopathology.js
+++ app/assets/javascripts/analyses/histopathology.js
@@ -13,7 +13,27 @@ LMML.loaders.analysis_histopathology = function () {
       add_analysis_histopathology_on_organ:
         LMML.add_('analysis_histopathology_on_organ', model),
       delete_analysis_histopathology_on_organ:
-        LMML.delete_('analysis_histopathology_on_organ', model)
+        LMML.delete_('analysis_histopathology_on_organ', model),
+      deleteOtherOrgan (event) {
+        var histo = this._findAnalysisByOrgan(event.currentTarget.value)
+        if (histo) {
+          histo.other_organ = ''
+        }
+      },
+      deleteOrganId (event) {
+        var histo = this._findAnalysisByOrgan(event.currentTarget.value)
+        if (histo) {
+          histo.organ_id = ''
+        }
+      },
+      _findAnalysisByOrgan (organValue) {
+        return this.analysis_histopathology_on_organs_attributes.find(
+          histo => {
+            return histo.organ_id + '' === organValue + '' ||
+                   histo.other_organ + '' === organValue + ''
+          }
+        )
+      }
     }
   })
 }
