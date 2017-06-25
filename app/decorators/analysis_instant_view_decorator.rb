3c88a24e5fbe5a49350c852e1da68f41778f0ef3
Malik Olivier Boussejra <malik@boussejra.com>
2017-06-25 17:38:31 +0900

    AnalysisInstantView: Move text from code to yml

--- app/decorators/analysis_instant_view_decorator.rb
+++ app/decorators/analysis_instant_view_decorator.rb
@@ -2,13 +2,12 @@ class AnalysisInstantViewDecorator < AnalysisBaseDecorator
   decorates_association :instant_view_drug_results
 
   def description
-    description = '本屍の尿につき検査キット添付の検査方法に準じてインスタントビュー濫用薬物スクリーニングをおこなったところ、'
-    if instant_view_drug_results.none_positive?
-      description += '全ての薬物領域には明らかな陽性反応は出現しなかった。'
-    else
-      description += "#{instant_view_drug_results.positive_drugs_description}陽性対照領域に明瞭な反応が出現する一方で、" \
-                     'それ以外の全ての薬物領域には明らかな陽性反応は出現しなかった。'
-    end
-    description
+    result = if instant_view_drug_results.none_positive?
+               t('.no_positive')
+             else
+               drugs = instant_view_drug_results.positive_drugs_description
+               t('.has_positive', drugs_description: drugs)
+             end
+    t('.complete_description', result: result)
   end
 end
