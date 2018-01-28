215cecb2aea6b4e6d2cda160690f5c636a41f7b1
Malik Olivier Boussejra <malik@boussejra.com>
2018-01-29 02:01:09 +0900

    InternalCraniumExaminationDecorator: Move sentences to locales

---
+++ app/decorators/internal_brain_examination_decorator.rb
@@ -0,0 +1,27 @@
+class InternalBrainExaminationDecorator < ExaminationBaseDecorator
+  def weight_description
+    return if object.weight.blank?
+    t('.weight', weight: object.weight)
+  end
+
+  def pia_congestion_description
+    return if object.pia_congestion.blank?
+    t('.pia_congestion',
+      intensity: object.translated_enum_value(:pia_congestion))
+  end
+
+  def arachnoid_no_injury_description
+    return if examination.injuries.any?
+    t('.no_injury_arachnoid')
+  end
+
+  def blood_spots_description
+    return if object.blood_spots.blank?
+    t('.blood_spots', quantity: object.translated_enum_value(:blood_spots))
+  end
+
+  def no_injury_remaining_description
+    return if examination.injuries.any?
+    t('.no_injury')
+  end
+end
