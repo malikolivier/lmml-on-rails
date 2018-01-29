3d2bfa2a8681ac5668dd1cdb4102e89afd078446
Malik Olivier Boussejra <malik@boussejra.com>
2018-01-29 13:37:58 +0900

    InternalAbdominalWallExaminationDecorator: Move text generation to decorator

--- app/models/internal_abdominal_wall_examination.rb
+++ app/models/internal_abdominal_wall_examination.rb
@@ -45,32 +45,8 @@ class InternalAbdominalWallExamination < ExaminationBase
     diaphragm_height_right.present? || diaphragm_height_left.present?
   end
 
-  def diaphragm_height_description
-    if diaphragm_height_left.present? &&
-       diaphragm_height_left == diaphragm_height_right
-      "左右#{rib_position_sentence(:left)}"
-    else
-      phrases = []
-      if diaphragm_height_left.present?
-        phrases.push("左#{rib_position_sentence(:left)}")
-      end
-      if diaphragm_height_right.present?
-        phrases.push("右#{rib_position_sentence(:right)}")
-      end
-      phrases.to_sentence two_words_connector: '、'
-    end
-  end
-
-  private
-
-  def between_rib?(deixis)
-    height = send("diaphragm_height_#{deixis}")
-    height % 1 > 0.25 && height % 1 <= 0.75
-  end
-
-  def rib_position_sentence(deixis)
-    height = send("diaphragm_height_#{deixis}")
-    "第#{height.truncate}#{between_rib?(deixis) ? '肋間' : '肋骨'}"
+  def same_diaphragm_height?
+    any_diaphragm_height? && diaphragm_height_left == diaphragm_height_right
   end
 
   accepts_nested_attributes_for :pleura_foreign_fluids,
