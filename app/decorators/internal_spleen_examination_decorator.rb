d073df93a43616f2a23ef6299929ac09e83bb432
Malik Olivier Boussejra <malik@boussejra.com>
2018-02-01 16:30:16 +0900

    InternalSpleenExaminationDecorator: Move text to decorator

---
+++ app/decorators/internal_spleen_examination_decorator.rb
@@ -0,0 +1,30 @@
+class InternalSpleenExaminationDecorator < ExaminationBaseDecorator
+  def weight_description
+    return if object.weight.blank?
+    t('.weight', weight: object.weight)
+  end
+
+  def hardness_congestion_description
+    phrases = PhraseBuilder.new(only_comma: true)
+    phrases << hardness_chunk
+    phrases << congestion_chunk
+    phrases.to_sentence
+  end
+
+  def pulp_distinct_description
+    return if object.pulp_distinct.blank?
+    "赤脾髄・白脾髄の別は#{I18n.t "clarity.#{object.pulp_distinct}"}。"
+  end
+
+  private
+
+  def hardness_chunk
+    return if object.hardness.blank?
+    t('.hardness', hardness: object.translated_enum_value(:hardness))
+  end
+
+  def congestion_chunk
+    return if object.congestion.blank?
+    t('.congestion', intensity: object.translated_enum_value(:congestion))
+  end
+end
--- app/models/internal_spleen_examination.rb
+++ app/models/internal_spleen_examination.rb
@@ -18,16 +18,4 @@ class InternalSpleenExamination < ExaminationBase
        i18n_key: :intensity
   enum pulp_distinct: %i[clear unclear unknown], _prefix: true,
        i18n_key: :clarity
-
-  def description
-    description = ''
-    phrases = []
-    phrases.push("硬度は#{I18n.t "hardness.#{hardness}"}") if hardness.present?
-    phrases.push("鬱血#{I18n.t "intensity.#{congestion}"}") if congestion.present?
-    description += phrases.to_sentence_with_comma_and_full_stop
-    if pulp_distinct.present?
-      description += "赤脾髄・白脾髄の別は#{I18n.t "clarity.#{pulp_distinct}"}。"
-    end
-    description
-  end
 end
