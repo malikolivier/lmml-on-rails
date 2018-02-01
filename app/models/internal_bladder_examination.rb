67007ba4015908cf997145b21eab11b588a93f05
Malik Olivier Boussejra <malik@boussejra.com>
2018-02-02 03:27:36 +0900

    InternalBladderExamination: Set i18n_key and use smart_slect
    Delete now useless helper.

--- app/models/internal_bladder_examination.rb
+++ app/models/internal_bladder_examination.rb
@@ -14,9 +14,10 @@
 
 class InternalBladderExamination < ExaminationBase
   enum urine_color: %i[unknown yellowish yellow amber red
-                       brown], _prefix: true
-  enum urine_transparency: %i[unknown transparent opaque], _prefix: 'urine'
-  enum membrane_color: %i[unknown pale], _prefix: true
+                       brown], _prefix: true, i18n_key: :color
+  enum urine_transparency: %i[unknown transparent opaque], _prefix: 'urine',
+       i18n_key: :transparency
+  enum membrane_color: %i[unknown pale], _prefix: true, i18n_key: :color
 
   def description
     description = content_description
