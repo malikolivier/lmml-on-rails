5447c0898ba17c11d0bc5fc3cc55983f7522b6d6
Malik Olivier Boussejra <malik@boussejra.com>
2018-02-01 16:39:10 +0900

    InternalPancreasExaminationDecorator: Move text to decorator

--- app/models/internal_pancreas_examination.rb
+++ app/models/internal_pancreas_examination.rb
@@ -11,11 +11,4 @@
 
 class InternalPancreasExamination < ExaminationBase
   enum hardness: Settings.enums.hardness
-
-  def description
-    description = ''
-    description += "硬度は#{I18n.t "hardness.#{hardness}"}。" if hardness.present?
-    description += '肉眼的に出血・脂肪壊死の異常なし。' if examination.injuries.none?
-    description
-  end
 end
