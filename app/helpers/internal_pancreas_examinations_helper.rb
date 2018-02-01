ec31ce429d6bee973413fb878819c7c89cff47b7
Malik Olivier Boussejra <malik@boussejra.com>
2018-02-01 16:38:08 +0900

    InternalPancreasExamination: Use smart_select
    Delete verbose helper.

--- app/helpers/internal_pancreas_examinations_helper.rb
+++
@@ -1,7 +0,0 @@
-module InternalPancreasExaminationsHelper
-  def translated_internal_pancreas_hardnesses
-    InternalPancreasExamination.hardnesses.map do |hardness, _|
-      [t("hardness.#{hardness}"), hardness]
-    end
-  end
-end
