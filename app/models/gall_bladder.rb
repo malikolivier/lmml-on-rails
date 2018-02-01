35943299d7a0dcffa0919de41355369f08b5f45c
Malik Olivier Boussejra <malik@boussejra.com>
2018-02-01 17:09:45 +0900

    GallBladder: Set i18n_key and use smart_select
    Delete now useless GallBladdersHelper.

--- app/models/gall_bladder.rb
+++ app/models/gall_bladder.rb
@@ -16,12 +16,14 @@
 class GallBladder < ApplicationRecord
   belongs_to :internal_liver_examination, required: true
 
-  enum bile_color: %i[dark_green yellowish brown], _prefix: 'bile'
-  enum bile_quantity: Settings.enums.five_scale_mass_quantity, _prefix: 'bile'
+  enum bile_color: %i[dark_green yellowish brown], _prefix: 'bile',
+       i18n_key: :color
+  enum bile_quantity: Settings.enums.five_scale_mass_quantity, _prefix: 'bile',
+       i18n_key: :mass_quantity
   enum gallstones_quantity: Settings.enums.five_scale_quantity,
-       _prefix: 'gallstones'
+       _prefix: 'gallstones', i18n_key: :quantity
   enum gallstones_color: %i[yellow yellow_green green brown],
-       _prefix: 'gallstones'
+       _prefix: 'gallstones', i18n_key: :color
   enum gallstones_size: Settings.enums.size,
-       _prefix: 'gallstones'
+       _prefix: 'gallstones', i18n_key: :size
 end
