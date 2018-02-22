4b49c3f38f1df95d84fc269657cdb3cf0844e1db
Malik Olivier Boussejra <malik@boussejra.com>
2018-02-22 13:16:40 +0900

    CreateChemicalAnalysisMethods: Disable cop CreateTableWithTimestamps
    This is a false positive, a bug in rubocop. Will be fixed in the next
    release: https://github.com/bbatsov/rubocop/issues/5503

--- db/migrate/20161229142000_create_chemical_analysis_methods.rb
+++ db/migrate/20161229142000_create_chemical_analysis_methods.rb
@@ -1,6 +1,9 @@
 class CreateChemicalAnalysisMethods < ActiveRecord::Migration[5.0]
   def change
+    # rubocop:disable Rails/CreateTableWithTimestamps
+    # TODO: rubocop bug fixed in https://github.com/bbatsov/rubocop/issues/5503
     create_table :chemical_analysis_methods, &:timestamps
+    # rubocop:enable Rails/CreateTableWithTimestamps
 
     reversible do |dir|
       dir.up do
