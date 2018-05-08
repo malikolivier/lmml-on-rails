5762ac49441889823a59cb344e7ed4443097ee05
ynakagawa <nakagawa.yuuki@ark-info-sys.co.jp>
2018-05-08 11:44:23 +0900

    js_lint修正(スペース挿入)

--- app/assets/javascripts/autopsy_filtering.js
+++ app/assets/javascripts/autopsy_filtering.js
@@ -14,9 +14,9 @@ LMML.loaders.autopsy_filtering = function () {
     var number = $('#filterNumber').val()
     var examiner = $('#filterExaminer').val()
     var policeInspector = $('#filterPoliceInspector').val()
-    var a = $('<a>', {
-      css:{ display: 'none'},
-      id:'link',
+    var a = $('<a>', { 
+      css:{ display: 'none'}, 
+      id:'link', 
       href:'./autopsies?number=' + number + '&examiner_id=' + examiner + '&police_inspector_id=' + policeInspector
     })
     $('body').append(a)
