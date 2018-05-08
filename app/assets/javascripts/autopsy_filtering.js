b381ccebf8fad6347ce95c0c83f36067e8004b74
ynakagawa <nakagawa.yuuki@ark-info-sys.co.jp>
2018-05-08 11:28:01 +0900

    js_lintエラー修正

--- app/assets/javascripts/autopsy_filtering.js
+++ app/assets/javascripts/autopsy_filtering.js
@@ -2,24 +2,24 @@
 
 $(function () {
   if (document.getElementById('autopsy_filtering') !== null) {
-    LMML.loaders.autopsy_filtering();
+    LMML.loaders.autopsy_filtering()
   }
 })
 
 LMML.loaders.autopsy_filtering = function () {
-  $('#autopsy_filtering').on('click',function(){
-    if(document.getElementById('link') !== null){
-      $('#link').remove();
+  $('#autopsy_filtering').on('click', function () {
+    if (document.getElementById('link') !== null) {
+      $('#link').remove()
     }
-    var number = $('#filterNumber').val();
-    var examiner = $('#filterExaminer').val();
-    var policeInspector = $('#filterPoliceInspector').val();
-    var a = $('<a>',{
-       css:{display:'none'},
-       id:'link',
-       href:'./autopsies?number='+number+'&examiner_id='+examiner+'&police_inspector_id='+policeInspector
-    });
-    $('body').append(a);
-    $('#link')[0].click();
-  });
+    var number = $('#filterNumber').val()
+    var examiner = $('#filterExaminer').val()
+    var policeInspector = $('#filterPoliceInspector').val()
+    var a = $('<a>', {
+      css:{ display: 'none'},
+      id:'link',
+      href:'./autopsies?number=' + number + '&examiner_id=' + examiner + '&police_inspector_id=' + policeInspector
+    })
+    $('body').append(a)
+    $('#link')[0].click()
+  })
 }
